9242b5e0913cd371a36b949d47f0fe240ae1e40d
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2020-09-25 22:05:12 +1200

    Return early on no tag or non-ndef

--- includes/ndef.h
+++ includes/ndef.h
@@ -20,38 +20,42 @@ class NDEFTagReader : public PollingComponent, public TextSensor {
   }
 
   void update() override {
-    if (this->nfc_->tagPresent()) {
-      ESP_LOGD(TAG, "Tag Found!");
-      NfcTag tag = this->nfc_->read();
+    if (!this->nfc_->tagPresent()) {
+      return;
+    }
 
-      if (tag.hasNdefMessage()) {
-        ESP_LOGD(TAG, "Tag has NDEF");
-        NdefMessage message = tag.getNdefMessage();
-        int recordCount = message.getRecordCount();
-        ESP_LOGD(TAG, "Tag has %d records", recordCount);
-        for (int i = 0; i < recordCount; i++) {
-          NdefRecord record = message.getRecord(i);
+    ESP_LOGD(TAG, "Tag Found!");
+    NfcTag tag = this->nfc_->read();
 
-          ESP_LOGD(TAG, "Record %d", (i+1));
-          ESP_LOGD(TAG, "Type: %s", record.getType().c_str());
+    if (!tag.hasNdefMessage()) {
+      ESP_LOGD(TAG, "No NDEF");
+      // TODO handle non-ndef tags by just sending the uid
+      return;
+    }
 
-          int payloadLength = record.getPayloadLength();
-          byte payload[payloadLength];
-          record.getPayload(payload);
+    ESP_LOGD(TAG, "Tag has NDEF");
+    NdefMessage message = tag.getNdefMessage();
+    int recordCount = message.getRecordCount();
+    ESP_LOGD(TAG, "Tag has %d records", recordCount);
+    for (int i = 0; i < recordCount; i++) {
+      NdefRecord record = message.getRecord(i);
 
-          std::string payloadAsString;
-          for (int c = 0; c < payloadLength; c++) {
-            payloadAsString += (char)payload[c];
-          }
-          ESP_LOGD(TAG, "Payload: %s", payloadAsString.c_str());
-          size_t pos = payloadAsString.find(TAG_PREFIX);
-          if (pos != std::string::npos) {
-            this->publish_state(payloadAsString.substr(pos + TAG_PREFIX.length()));
-            this->set_timeout("tag_clear", 1000, [this]() { this->publish_state(""); });
-          }
-        }
-      } else {
-        ESP_LOGD(TAG, "No NDEF");
+      ESP_LOGD(TAG, "Record %d", (i+1));
+      ESP_LOGD(TAG, "Type: %s", record.getType().c_str());
+
+      int payloadLength = record.getPayloadLength();
+      byte payload[payloadLength];
+      record.getPayload(payload);
+
+      std::string payloadAsString;
+      for (int c = 0; c < payloadLength; c++) {
+        payloadAsString += (char)payload[c];
+      }
+      ESP_LOGD(TAG, "Payload: %s", payloadAsString.c_str());
+      size_t pos = payloadAsString.find(TAG_PREFIX);
+      if (pos != std::string::npos) {
+        this->publish_state(payloadAsString.substr(pos + TAG_PREFIX.length()));
+        this->set_timeout("tag_clear", 1000, [this]() { this->publish_state(""); });
       }
     }
   }
